0ab3684bcc88decd3b44e0167db2ee4529f3e371
denirahman <122171498+jangoden@users.noreply.github.com>
2025-10-17 22:05:24 +0700

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -1,10 +1,4 @@
 export const siteConfig = {
-  // Diperbarui: Menambahkan /api di akhir URL
-  apiUrl: process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000",
-  
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000",
-  siteName: process.env.NEXT_PUBLIC_SITE_NAME || "Warung Digital", // Bisa disesuaikan
-  description:
-    process.env.NEXT_PUBLIC_SITE_DESCRIPTION ||
-    "Warung Digital menyediakan layanan pembuatan website, desain grafis, dan optimasi SEO untuk membantu bisnis Anda tumbuh di dunia digital.",
+  siteUrl: 'https://warungnyaweb.my.id',
+  apiUrl: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://backend.warungnyaweb.biz.id',
 };
